2b92504ec84c86110fb67c5f8ffea504691f09a3
ayabarrera <alyannabarrera@gmail.com>
2025-01-20 18:13:29 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    japanhotel.addEventListener("click", () =>  {
+        window.open("https://www.booking.com/searchresults.html?ss=Kamakura%2C+Kanagawa%2C+Japan&ssne=Kamakura&ssne_untouched=Kamakura&efdco=1&label=gog235jc-1DCAcoJ0IVZWRtb250b25fd2VzdGVkbW9udG9uSDNYA2gniAEBmAExuAEHyAEM2AED6AEB-AECiAIBqAIDuALqsbC8BsACAdICJGQ5ZGY3MmUyLTQ1NTItNDk1NS04MjkwLTg0ODA3M2FmYWU4ZtgCBOACAQ&sid=ddcfe9b0443bfc016661958002ff753d&aid=356980&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-231760&dest_type=city&place_id=city%2F-231760&ac_position=0&ac_click_type=b&ac_langcode=en&ac_suggestion_list_length=5&search_selected=true&search_pageview_id=6a7f9504f9ee03a0&ac_meta=GhBmYWYzOTUxNDJhODIwNmIyIAAoATICZW46CUthbWFrdXJhIEAASgBQAA%3D%3D&checkin=2025-02-21&checkout=2025-02-22&ltfd=5%3A1%3A4-2025%3A1%3A&group_adults=2&no_rooms=1&group_children=0");
+    });
+
+    usahotel.addEventListener("click", function(event) {
+        window.open("https://www.booking.com/searchresults.html?ss=Kamakura%2C+Kanagawa%2C+Japan&ssne=Kamakura&ssne_untouched=Kamakura&efdco=1&label=gog235jc-1DCAcoJ0IVZWRtb250b25fd2VzdGVkbW9udG9uSDNYA2gniAEBmAExuAEHyAEM2AED6AEB-AECiAIBqAIDuALqsbC8BsACAdICJGQ5ZGY3MmUyLTQ1NTItNDk1NS04MjkwLTg0ODA3M2FmYWU4ZtgCBOACAQ&sid=ddcfe9b0443bfc016661958002ff753d&aid=356980&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-231760&dest_type=city&place_id=city%2F-231760&ac_position=0&ac_click_type=b&ac_langcode=en&ac_suggestion_list_length=5&search_selected=true&search_pageview_id=6a7f9504f9ee03a0&ac_meta=GhBmYWYzOTUxNDJhODIwNmIyIAAoATICZW46CUthbWFrdXJhIEAASgBQAA%3D%3D&checkin=2025-02-21&checkout=2025-02-22&ltfd=5%3A1%3A4-2025%3A1%3A&group_adults=2&no_rooms=1&group_children=0");
+    });
+
+    canadahotel.addEventListener("click", function(event) {
+        window.open("https://www.booking.com/searchresults.html?ss=chilliwack&ssne=Sequoia+National+Park&ssne_untouched=Sequoia+National+Park&label=gog235jc-1DCAcoJ0IVZWRtb250b25fd2VzdGVkbW9udG9uSDNYA2gniAEBmAExuAEHyAEM2AED6AEB-AECiAIBqAIDuALqsbC8BsACAdICJGQ5ZGY3MmUyLTQ1NTItNDk1NS04MjkwLTg0ODA3M2FmYWU4ZtgCBOACAQ&sid=ddcfe9b0443bfc016661958002ff753d&aid=356980&lang=en-us&sb=1&src_elem=sb&src=searchresults&dest_id=-562560&dest_type=city&place_id=city%2F-562560&ac_position=0&ac_click_type=b&ac_langcode=en&ac_suggestion_list_length=5&search_selected=true&search_pageview_id=6f0a95ec1d5206d4&ac_meta=GhBhZjQwOTVmOTE4MWMwODNlIAAoATICZW46C2NoaWxsaXdhY2snQABKAFAA&checkin=2025-04-04&checkout=2025-04-05&ltfd=5%3A1%3A4-2025%3A1%3A&group_adults=2&no_rooms=1&group_children=0");
+    });
+
+
+});
+
